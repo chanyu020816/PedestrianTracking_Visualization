1b746b6404f944067341b8ab41f55eaf419de6cd
chanyu <trafalgarlaw0816@gmail.com>
2024-04-01 17:37:20 +0800

    Adding background image

--- GUI/utils/ImageConverter.py
+++ GUI/utils/ImageConverter.py
@@ -3,13 +3,13 @@ from os.path import basename
 
 def pic2str(file):
     pic = open(file, 'rb')
-    name = basename(file)
+    name = basename(file)[:-4]
     content = '{} = {}\n'.format(name, base64.b64encode(pic.read()))
     pic.close()
 
-    with open('ConvertedImages.py', 'a') as f:
+    with open('../Base64Image.py', 'a') as f:
         f.write(content)
 
 
 if __name__ == '__main__':
-    pic2str("../images/")
+    pic2str("../images/PedestrianTrackingPageBG.png")
---
+++ ObjectDetectionModel/data/utils/ImageAugmentation.py
@@ -0,0 +1,3 @@
+from workflow import run_pipeline
+
+run_pipeline('./contants.yaml')
