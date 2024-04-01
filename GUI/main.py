3bc71f06afae82c3c9472ae7aca7a879c31c307b
chanyu <trafalgarlaw0816@gmail.com>
2024-04-01 17:38:55 +0800

    Add pedestrian tracking page

--- GUI/main.py
+++ GUI/main.py
@@ -1,14 +1,22 @@
 import tkinter as tk
-
-
+from Constant import *
+from PedestrianTrackingPage import PedestrianTrackingPage
 class MainView(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         self.title("Pedestrian Tracking Visualization")
-        self.geometry("1400x800")
+        self.geometry(f"{WIDTH}x{HEIGHT}")
         self.container = tk.Frame(self)
         self.container.grid(row=0, column=0, sticky="nsew")
 
+        self.pages = {
+            "PedestrianTrackingPage": PedestrianTrackingPage(self.container, self),
+        }
+
+        self.initial()
+
+    def initial(self):
+        self.pages["PedestrianTrackingPage"].grid(row=0, column=0, sticky="nsew")
 
 if __name__ == "__main__":
     app = MainView()
